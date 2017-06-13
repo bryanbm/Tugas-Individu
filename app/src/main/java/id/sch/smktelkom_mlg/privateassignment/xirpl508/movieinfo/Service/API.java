ee86c9fb0dde7cc37c13929ca57f8bb9b5189a41
Bryan <bryan_marsono24rpl@student.smktelkom-mlg.sch.id>
2017-06-13 12:27:44 +0800

    FInal

---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl508/movieinfo/Service/API.java
@@ -0,0 +1,6 @@
+package id.sch.smktelkom_mlg.privateassignment.xirpl508.movieinfo.Service;
+
+public class API {
+    static public String API_URL = "http://api.themoviedb.org/3/discover/movie";
+    static public String API_KEY = "372cb8bdb5d3b6605fa95e3d4ff5f8d4";
+}
